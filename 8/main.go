2efb6b1a5d28f4e75f11ab32c5cbcf8977827175
semvis123 <semvisscher10@gmail.com>
2023-12-10 01:30:35 +0100

    Fix and cleanup day 8

--- 8/main.go
+++ 8/main.go
@@ -6,12 +6,11 @@ import (
 	"strings"
 
 	"golang.org/x/exp/maps"
-	"golang.org/x/exp/slices"
 )
 
 func main() {
 	lines := aoc.GetInput()
-	// log.Printf("part 1: %d", part_1(lines))
+	log.Printf("part 1: %d", part_1(lines))
 	log.Printf("part 2: %d", part_2(lines))
 }
 
@@ -54,68 +53,24 @@ func part_2(input []string) int {
 	instructions, network := readNodes(input)
 
 	curr := aoc.Filter(maps.Keys(network), func(k string) bool { return k[2] == 'A' })
-	type state struct {
-		instr int
-		steps int
-		node  string
-	}
-	log.Printf("%v", curr)
 
-	offsets := []int{}
-	distances := []int{}
-	distancesToZ := []int{}
+	stepsToZ := []int{}
 	for i := range curr {
-		seen := []state{{0, 0, curr[i]}}
-
-		steps := int(0)
-		stepsZ := int(0)
-		for ; (!slices.ContainsFunc(seen, func(x state) bool {
-			return x.instr == (steps)%int(len(instructions)) && x.node == curr[i]
-		})) || steps == 0; steps++ {
-			name := curr[i]
-			n := network[name]
+		steps := 0
+		for ; ; steps++ {
+			n := network[curr[i]]
+			if curr[i][2] == 'Z' {
+				break
+			}
 			if instructions[steps%int(len(instructions))] {
 				curr[i] = n.left
 			} else {
 				curr[i] = n.right
 			}
-			seen = append(seen, state{steps, (steps + 1) % int(len(instructions)), name})
-			if name[2] == 'Z' {
-				if stepsZ == 0 {
-					stepsZ = steps
-				}
-			}
 		}
 
-		var looping *state = nil
-		for _, x := range seen {
-			if x.node == curr[i] && x.instr == steps%int(len(instructions)) {
-				looping = &x
-			}
-		}
-		if looping == nil {
-			panic("hmmmm")
-		}
-
-		offsets = append(offsets, looping.steps)
-		distances = append(distances, steps-looping.steps)
-		distancesToZ = append(distancesToZ, stepsZ-looping.steps)
-	}
-	log.Printf("%v", offsets)
-	log.Printf("%v", distances)
-	log.Printf("%v", distancesToZ)
-
-	// something's off here, not sure what
-	steps := 0
-	for i := range curr {
-		d := aoc.RemoveIndex(distances, i)
-		if steps < offsets[i] {
-			steps += aoc.LCM(1, offsets[i], d...)
-		}
-		if (steps-offsets[i])%distances[i] != distancesToZ[i] {
-			steps += aoc.LCM(1, distancesToZ[i], d...)
-		}
+		stepsToZ = append(stepsToZ, steps)
 	}
 
-	return steps
+	return aoc.LCM(1, 1, stepsToZ...)
 }
